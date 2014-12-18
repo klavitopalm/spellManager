9f7f5afe2dbf543219fb8937e604f4dead5ccad4
klavitopalm <michael_eberl@gmx.net>
2014-12-18 18:44:55 +0100

    added basic styling for spells

--- public/js/controllers/ClassesCtrl.js
+++ public/js/controllers/ClassesCtrl.js
@@ -9,12 +9,15 @@ angular.module('ClassesCtrl', []).controller('ClassesController', function($http
       console.log('Error: ' + data);
    });
 
+   var activeClassId;
+
    $scope.showSpells = function(id) {
       $http.get('/api/classspells/'+id)
       .success(function(data) {
          var temp = data[0];
          $scope.selectedClassSpells = temp.spells;
          $scope.selectedSpell = '';
+         activeClassId = id;
          console.log(data);
       })
       .error(function(data) {
@@ -25,5 +28,16 @@ angular.module('ClassesCtrl', []).controller('ClassesController', function($http
    $scope.showSpellDetails = function(spell) {
       $scope.selectedSpell = spell;
    }
+
+
+   $scope.isActiveClass = function (id) {
+      var active = (id === activeClassId);
+      return active;
+   };
+
+   $scope.isActiveSpell = function (id) {
+      var active = (id === $scope.selectedSpell._id);
+      return active;
+   };
 
 });
