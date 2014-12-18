54440a49d45fb27c574ebfb4ef6c4eec83bebc9b
klavitopalm <michael_eberl@gmx.net>
2014-12-18 23:27:38 +0100

    spells are now displayed regarding their level

--- public/js/controllers/ClassesCtrl.js
+++ public/js/controllers/ClassesCtrl.js
@@ -1,4 +1,5 @@
-angular.module('ClassesCtrl', ['ClassesWithSpellsService', 'ClassesService']).controller('ClassesController', function($http, $scope, ClassesWithSpells, Classes) {
+angular.module('ClassesCtrl', ['ClassesWithSpellsService', 'ClassesService'])
+   .controller('ClassesController', function($http, $scope, ClassesWithSpells, Classes) {
 
 
    Classes.get(function(data) {
@@ -8,8 +9,74 @@ angular.module('ClassesCtrl', ['ClassesWithSpellsService', 'ClassesService']).co
    var activeClassId;
 
    $scope.showSpells = function(id) {
-   ClassesWithSpells.get(id, function(data) {
-      $scope.testAccounts = data;
+      ClassesWithSpells.get(id, function(data) {
+         var spellsLevel0 = [];
+         var spellsLevel1 = [];
+         var spellsLevel2 = [];
+         var spellsLevel3 = [];
+         var spellsLevel4 = [];
+         var spellsLevel5 = [];
+         var spellsLevel6 = [];
+         var spellsLevel7 = [];
+         var spellsLevel8 = [];
+         var spellsLevel9 = [];
+         var spellsLevel10 = [];
+
+         var singleClass = data[0];
+         angular.forEach(singleClass.spells, function(singleSpell) {
+
+
+            switch (singleSpell.level) {
+               case 0:
+                  spellsLevel0.push(singleSpell);
+               break;
+               case 1:
+                  spellsLevel1.push(singleSpell);
+               break;
+               case 2:
+                  spellsLevel2.push(singleSpell);
+               break;
+               case 3:
+                  spellsLevel3.push(singleSpell);
+               break;
+               case 4:
+                  spellsLevel4.push(singleSpell);
+               break;
+               case 5:
+                  spellsLevel5.push(singleSpell);
+               break;
+               case 6:
+                  spellsLevel6.push(singleSpell);
+               break;
+               case 7:
+                  spellsLevel7.push(singleSpell);
+               break;
+               case 8:
+                  spellsLevel8.push(singleSpell);
+               break;
+               case 9:
+                  spellsLevel9.push(singleSpell);
+               break;
+               case 10:
+                  spellsLevel10.push(singleSpell);
+               break;
+               default:
+                  console.log('Error: unsupported spell level -' + singleSpell.level);
+            }
+         });
+
+         $scope.spellsLevel0 = spellsLevel0;
+         $scope.spellsLevel1 = spellsLevel1;
+         $scope.spellsLevel2 = spellsLevel2;
+         $scope.spellsLevel3 = spellsLevel3;
+         $scope.spellsLevel4 = spellsLevel4;
+         $scope.spellsLevel5 = spellsLevel5;
+         $scope.spellsLevel6 = spellsLevel6;
+         $scope.spellsLevel7 = spellsLevel7;
+         $scope.spellsLevel8 = spellsLevel8;
+         $scope.spellsLevel9 = spellsLevel9;
+         $scope.spellsLevel10 = spellsLevel10;
+
          var temp = data[0];
          $scope.selectedClassSpells = temp.spells;
          $scope.selectedSpell = '';
