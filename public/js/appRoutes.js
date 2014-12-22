81d382bbc83befbe2a769dcfe317d2b5973993cc
klavitopalm <michael_eberl@gmx.net>
2014-12-22 18:05:58 +0100

    newlines in spell details now vie pre-tag

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -3,12 +3,12 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider',
 
    $routeProvider
 
-   .when('/', {
+   .when('/spells', {
       templateUrl: 'views/classes.html',
       controller: 'ClassesController',
    })
 
-   .when('/spells', {
+   .when('/', {
       templateUrl: 'views/spells.html',
       controller: 'SpellsController'
    });
